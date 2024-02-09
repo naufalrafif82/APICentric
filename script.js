c433e38e39944f0559bb8c43f21e2b258fd25b43
naufalrafif82 <101546592+naufalrafif82@users.noreply.github.com>
2024-02-09 15:28:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,11 @@ formToken.addEventListener("submit", async (event) => {
         password,
     };
 
-    const response = await fetch("https://busana-prod.centricsoftware.com/csi-requesthandler/api/v2/session", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    });
-
-    if (response.status === 200) {
-        const token = await response.json();
+    try {
+        const response = await axios.post("https://busana-prod.centricsoftware.com/csi-requesthandler/api/v2/session", data);
+        const token = response.data.token;
         result.textContent = `Token: ${token}`;
-    } else {
-        const error = await response.text();
-        result.textContent = `Error: ${error}`;
+    } catch (error) {
+        result.textContent = `Error: ${error.message}`;
     }
 });
